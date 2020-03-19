5f3d514b94690339f8ca73b9b8a86d2da95f1917
imayakulothungan <g52opal@yahoo.com>
2020-03-19 16:19:40 -0400

    Fix NLP '

--- case-study-school-budget-eda.py
+++ case-study-school-budget-eda.py
@@ -186,3 +186,29 @@ pl.fit(X_train, y_train)
 # Compute and print accuracy
 accuracy = pl.score(X_test, y_test)
 print("\nAccuracy on budget dataset: ", accuracy)
+
+# Import RandomForestClassifier
+
+# Add model step to pipeline: pl
+pl = Pipeline([
+    ('union', FeatureUnion(
+        transformer_list=[
+            ('numeric_features', Pipeline([
+                ('selector', get_numeric_data),
+                ('imputer', SimpleImputer())
+            ])),
+            ('text_features', Pipeline([
+                ('selector', get_text_data),
+                ('vectorizer', CountVectorizer())
+            ]))
+        ]
+    )),
+    ('clf', RandomForestClassifier(n_estimators=15))
+])
+
+# Fit to the training data
+pl.fit(X_train, y_train)
+
+# Compute and print accuracy
+accuracy = pl.score(X_test, y_test)
+print("\nAccuracy on budget dataset: ", accuracy)
